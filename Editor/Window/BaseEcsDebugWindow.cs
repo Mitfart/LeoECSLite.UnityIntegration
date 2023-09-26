d05e2b50cb5a51c6367e0d7e6a029e18701f67f5
Mitfart <mr.mitrum@mail.ru>
2023-09-26 17:06:14 +0300

    Updade to LeoECS 2023.9.22

--- Editor/Window/BaseEcsDebugWindow.cs
+++ Editor/Window/BaseEcsDebugWindow.cs
@@ -4,99 +4,98 @@ using Mitfart.LeoECSLite.UnityIntegration.Editor.Window.World;
 using UnityEditor;
 
 namespace Mitfart.LeoECSLite.UnityIntegration.Editor.Window {
-  public abstract class BaseEcsDebugWindow : EditorWindow, IEcsWorldEventListener {
-    public EcsWorldDebugSystem ActiveSystem { get; private set; }
+   public abstract class BaseEcsDebugWindow : EditorWindow, IEcsWorldEventListener {
+      public EcsWorldDebugSystem ActiveSystem { get; private set; }
 
 
 
-    private void OnEnable() {
-      EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
-      EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
-    }
-
-    private void OnDisable() {
-      EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
-    }
-
-    private void CreateGUI() {
-      CreateElements();
-      StructureElements();
-      InitElements();
-    }
-
-
-
-    public virtual void OnEntityCreated(int   e)      { }
-    public virtual void OnEntityChanged(int   entity) { }
-    public virtual void OnEntityDestroyed(int entity) { }
-
-    public virtual void OnWorldResized(int        newSize) { }
-    public virtual void OnWorldDestroyed(EcsWorld world)   { }
-
-    public virtual void OnFilterCreated(EcsFilter filter) { }
-
-
-
-    protected abstract void CreateElements();
-    protected abstract void StructureElements();
-    protected abstract void InitElements();
-
-
-    protected abstract void InitInspector();
-    protected abstract void ResetInspector();
-
-
-
-    protected void ChangeWorld(WorldTabData worldTab) {
-      if (ActiveDebugSystems.TryGet(worldTab.Name, out EcsWorldDebugSystem debugSystem))
-        SetActiveWorldDebugSystem(debugSystem);
-      else
-        throw new Exception($"Can't find System relative to `{worldTab}` world!");
-    }
-
-
-    private void SetActiveWorldDebugSystem(EcsWorldDebugSystem newActiveDebugSystem) {
-      if (ActiveSystem == newActiveDebugSystem)
-        return;
-
-      ResetActiveSystem();
-
-      ActiveSystem = newActiveDebugSystem;
-
-      InitActiveSystem();
-    }
-
-
-    private void InitActiveSystem() {
-      if (ActiveSystem == null)
-        return;
-
-      ActiveSystem.World.AddEventListener(this);
-      InitInspector();
-    }
-
-    private void ResetActiveSystem() {
-      if (ActiveSystem == null)
-        return;
-
-      ActiveSystem.World.RemoveEventListener(this);
-      ResetInspector();
-    }
-
-
-
-    private void OnPlayModeStateChanged(PlayModeStateChange state) {
-      switch (state) {
-        case PlayModeStateChange.EnteredEditMode:
-        case PlayModeStateChange.ExitingEditMode:
-        case PlayModeStateChange.EnteredPlayMode:
-          break;
-        case PlayModeStateChange.ExitingPlayMode:
-          ResetInspector();
-          break;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(state), state, null);
+      private void OnEnable() {
+         EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+         EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
       }
-    }
-  }
+
+      private void OnDisable() {
+         EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+      }
+
+      private void CreateGUI() {
+         CreateElements();
+         StructureElements();
+         InitElements();
+      }
+
+
+
+      public virtual void OnEntityCreated(int   e)                                { }
+      public virtual void OnEntityChanged(int   entity, short poolId, bool added) { }
+      public virtual void OnEntityDestroyed(int entity) { }
+
+      public virtual void OnWorldResized(int        newSize) { }
+      public virtual void OnWorldDestroyed(EcsWorld world)   { }
+
+      public virtual void OnFilterCreated(EcsFilter filter) { }
+
+
+
+      protected abstract void CreateElements();
+      protected abstract void StructureElements();
+      protected abstract void InitElements();
+
+
+      protected abstract void InitInspector();
+      protected abstract void ResetInspector();
+
+
+
+      protected void ChangeWorld(WorldTabData worldTab) {
+         if (ActiveDebugSystems.TryGet(worldTab.Name, out EcsWorldDebugSystem debugSystem))
+            SetActiveWorldDebugSystem(debugSystem);
+         else
+            throw new Exception($"Can't find System relative to `{worldTab}` world!");
+      }
+
+
+      private void SetActiveWorldDebugSystem(EcsWorldDebugSystem newActiveDebugSystem) {
+         if (ActiveSystem == newActiveDebugSystem)
+            return;
+
+         ResetActiveSystem();
+
+         ActiveSystem = newActiveDebugSystem;
+
+         InitActiveSystem();
+      }
+
+
+      private void InitActiveSystem() {
+         if (ActiveSystem == null)
+            return;
+
+         ActiveSystem.World.AddEventListener(this);
+         InitInspector();
+      }
+
+      private void ResetActiveSystem() {
+         if (ActiveSystem == null)
+            return;
+
+         ActiveSystem.World.RemoveEventListener(this);
+         ResetInspector();
+      }
+
+
+
+      private void OnPlayModeStateChanged(PlayModeStateChange state) {
+         switch (state) {
+            case PlayModeStateChange.EnteredEditMode:
+            case PlayModeStateChange.ExitingEditMode:
+            case PlayModeStateChange.EnteredPlayMode:
+               break;
+            case PlayModeStateChange.ExitingPlayMode:
+               ResetInspector();
+               break;
+            default: throw new ArgumentOutOfRangeException(nameof(state), state, null);
+         }
+      }
+   }
 }
--- Runtime/EcsWorldDebugSystem.cs
+++ Runtime/EcsWorldDebugSystem.cs
@@ -6,57 +6,57 @@ using Mitfart.LeoECSLite.UnityIntegration.Name;
 using Mitfart.LeoECSLite.UnityIntegration.View;
 
 namespace Mitfart.LeoECSLite.UnityIntegration {
-  public sealed class EcsWorldDebugSystem : IEcsPreInitSystem, IEcsRunSystem, IEcsWorldEventListener {
-    public string       DebugName    { get; }
-    public string       WorldName    { get; }
-    public EcsWorld     World        { get; private set; }
-    public WorldView    View         { get; private set; }
-    public NameSettings NameSettings { get; }
-    public int          WorldSize    { get; private set; }
+   public sealed class EcsWorldDebugSystem : IEcsPreInitSystem, IEcsRunSystem, IEcsWorldEventListener {
+      public string       DebugName    { get; }
+      public string       WorldName    { get; }
+      public EcsWorld     World        { get; private set; }
+      public WorldView    View         { get; private set; }
+      public NameSettings NameSettings { get; }
+      public int          WorldSize    { get; private set; }
 
 
 
-    public EcsWorldDebugSystem(string worldName = null, NameSettings nameSettings = null) {
-      WorldName    = worldName;
-      DebugName    = worldName.ToWorldDebugName();
-      NameSettings = nameSettings;
-    }
+      public EcsWorldDebugSystem(string worldName = null, NameSettings nameSettings = null) {
+         WorldName    = worldName;
+         DebugName    = worldName.ToWorldDebugName();
+         NameSettings = nameSettings;
+      }
 
-    public void PreInit(IEcsSystems systems) {
-      InitWorld(systems);
+      public void PreInit(IEcsSystems systems) {
+         InitWorld(systems);
 
-      View      = new WorldView(this, NameSettings);
-      WorldSize = World.GetWorldSize();
-      World.AddEventListener(this);
+         View      = new WorldView(this, NameSettings);
+         WorldSize = World.GetWorldSize();
+         World.AddEventListener(this);
 
-      ActiveDebugSystems.Register(this);
+         ActiveDebugSystems.Register(this);
 
-      InitEntities();
-    }
-
-
-
-    public void Run(IEcsSystems systems) => View.Refresh();
-
-    public void OnEntityCreated(int   e) => View.GetEntityView(e).Activate();
-    public void OnEntityChanged(int   e) { }
-    public void OnEntityDestroyed(int e) => View.GetEntityView(e).Deactivate();
-
-    public void OnWorldResized(int newSize) => View.Resize(WorldSize = newSize);
-
-    public void OnWorldDestroyed(EcsWorld world) {
-      View.Destroy();
-      World.RemoveEventListener(this);
-      ActiveDebugSystems.Unregister(this);
-    }
-
-    public void OnFilterCreated(EcsFilter filter) { }
+         InitEntities();
+      }
 
 
 
-    private void InitWorld(IEcsSystems systems) => World = systems.GetWorld(WorldName) ?? throw new Exception($"Cant find required world! ({WorldName})");
-    private void InitEntities()                 => World.ForeachEntity(OnEntityCreated);
-  }
+      public void Run(IEcsSystems systems) => View.Refresh();
+
+      public void OnEntityCreated(int   e)                                => View.GetEntityView(e).Activate();
+      public void OnEntityChanged(int   entity, short poolId, bool added) { }
+      public void OnEntityDestroyed(int e) => View.GetEntityView(e).Deactivate();
+
+      public void OnWorldResized(int newSize) => View.Resize(WorldSize = newSize);
+
+      public void OnWorldDestroyed(EcsWorld world) {
+         View.Destroy();
+         World.RemoveEventListener(this);
+         ActiveDebugSystems.Unregister(this);
+      }
+
+      public void OnFilterCreated(EcsFilter filter) { }
+
+
+
+      private void InitWorld(IEcsSystems systems) => World = systems.GetWorld(WorldName) ?? throw new Exception($"Cant find required world! ({WorldName})");
+      private void InitEntities()                 => World.ForeachEntity(OnEntityCreated);
+   }
 }
 
 #endif
